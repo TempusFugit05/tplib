8ba186522da3cab500047587b0aa40769947fc87
TempusFugit05 <adam.agrano@gmail.com>
2025-02-05 18:30:48 +0200

    renaming write_char and write_char_buffer to file_write

--- include/system/system_io.h
+++ include/system/system_io.h
@@ -2,4 +2,6 @@
 #define SYSTEM_IO_H
 void write_char_buffer(const char* const buffer, const size_t buffer_size, const int file);
 void write_char(const char character, const int file);
+void file_read(char* buffer, const size_t buffer_size, const int file);
+void std_read(char* buffer, const size_t buffer_size);
 #endif // SYSTEM_IO_H
--- src/linux/system_io.cpp
+++ src/linux/system_io.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include "system_io.h"
+
 void write_char_buffer(const char* const buffer, const size_t buffer_size, const int file)
 {
     write(file, buffer, buffer_size);
@@ -9,4 +10,14 @@ void write_char(const char character, const int file)
 {
     const char char_to_write[1] = {character};
     write(file, char_to_write, 1);
+}
+
+void file_read(char* buffer, const size_t buffer_size, const int file)
+{
+    read(file, buffer, buffer_size);
+}
+
+void std_read(char* buffer, const size_t buffer_size)
+{
+    file_read(buffer, buffer_size, 0);
 }
